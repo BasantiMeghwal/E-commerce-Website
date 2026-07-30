85103b83bc2dd40643718559c13b5f9ec3ee9b00
Basanti <Basanti@gmail.com>
2026-07-30 17:58:44 +0530

    Added login signup functionality and updated UI

--- app.js
+++ app.js
@@ -29,10 +29,15 @@ app.get('/categories', (req, res) => {
 app.get('/contact', (req, res) => {
     res.sendFile(path.join(__dirname, 'template', 'contact.html'));
 });
+app.get('/signup',(req,res)=>{
+    res.sendFile(path.join(__dirname, 'template' , 'signup.html'))
+})
 app.get('/success', (req, res) => {
     res.sendFile(path.join(__dirname, 'template', 'success_page.html'));
 });
-
+app.get('/signup_success', (req, res) => {
+    res.sendFile(path.join(__dirname, 'template', 'signup_success.html'));
+});
 
 const contactSchema = new mongoose.Schema({
     name: String,
@@ -55,6 +60,47 @@ app.post('/contact', async (req, res) => {
 
 
 });
+
+const signupSchema = new mongoose.Schema({
+  name: String,
+  email:String,
+  password:String
+});
+const signup = mongoose.model('Signup', signupSchema);
+
+app.post('/signup', async (req, res) => {
+    
+    const { name, email, password,confirmPassword } = req.body
+
+    if(password !== confirmPassword){
+        
+        return res.send("Password and Confirm Password do not match");
+    }
+    const newUser = new signup   ({
+        name: name,
+        email: email,
+        password: password
+    })
+    await newUser.save()
+    res.redirect('/signup_success');
+
+
+});
+
+app.post('/login',async(req,res)=>{
+    const {email,password} = req.body;
+
+    const user = await signup.findOne({
+        email:email,
+        password:password
+    })
+    if(user){
+        res.redirect('/')
+    }
+    else{
+        res.send("Invalid Email or Password");
+    }
+})
 
 app.listen(port, () => {
     console.log(`server running at http://localhost:${port}`);
